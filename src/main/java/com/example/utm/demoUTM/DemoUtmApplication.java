12078e83ca07c0acc583b3eb5d4dab5546b9037d
ipanasenco <p0w3r0v3r*>
2018-11-08 15:23:38 +0200

    first commit

---
+++ src/main/java/com/example/utm/demoUTM/DemoUtmApplication.java
@@ -0,0 +1,12 @@
+package com.example.utm.demoUTM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUtmApplication.class, args);
+	}
+}
